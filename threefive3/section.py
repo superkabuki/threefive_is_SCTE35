56d74a7a7278c104c8384b1adb6029cfbcd75ce7
folieamatt <drmattw@gmail.com>
2025-03-31 20:00:09 +0100

    Fix Tier
    Fixed tier so that it does not default to 0xFFF if one has already been set.

--- threefive3/section.py
+++ threefive3/section.py
@@ -213,8 +213,8 @@ class SpliceInfoSection(SCTE35Base):
         """
         encode SpliceInfoSection.tier
         """
-        #     if not self.tier:
-        self.tier = "0xfff"
+        if not self.tier:
+            self.tier = "0xfff"
         nbin.add_hex(self.tier, 12)
 
     def _encode_splice_command(self, nbin):
