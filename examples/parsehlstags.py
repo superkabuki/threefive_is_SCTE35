0a4b76122bf17804c20d4c97c35913701a6722af
Super Kabuki <a@iodisco.com>
2025-04-12 19:30:43 -0400

    json output

--- examples/parsehlstags.py
+++ examples/parsehlstags.py
@@ -2,26 +2,50 @@
 parsehlstags.py
 Use the TagParser class to parse hls tags from a m3u8 file
 and group the tags into a dict by segment.
+
+Example:
+these are the tags for the segment index_2_8875296.ts
+
+          "index_2_8875296.ts": {
+
+               "#EXT-X-PROGRAM-DATE-TIME": "2024-11-24T15:24:42.700Z",
+
+                "#EXT-X-CUE-OUT-CONT": {
+                      "SCTE35":     "/DBAAAAAAyiYAAAABQb/FOWM0AAqAihDVUVJ/////3//AAFJi8ABFG1zbmJjX0VQMDM0NDY3MTkxMDY5IgIEbMGIMA=",
+                      "Duration":     239.967,
+                      "ElapsedTime":    238.867,
+                      "CAID":  624854195548842227305490153684004159669692020281
+                },
+
+            "#EXTINF":  1.133
+          },
 """
-
+import json
+import sys
 from threefive3.hlstags import TagParser
-
+from threefive3.new_reader import reader
 
 data={}
-with open('/home/a/flat.m3u8','r') as mu:
-    results=[]
-    lines= mu.readlines()
-    for line in lines:
-        if line.startswith("#"):
-            # hold tags until we find a segment
-            results.append(line)
-        else:
-            tp=TagParser(results)
-            # when we find a segment, make it a key in data
-            # and a dict of the tags as the value
-            data[line]=tp.tags
-            # clear results for next segment
-            results =[]
-
-    for k in list(sorted(data.keys())):
-        print(f'{k} : {data[k]}')
+for arg in sys.argv[1:]:
+    with reader(arg) as mu:
+        results=[]
+        lines= mu.readlines()
+        for line in lines:
+            line=line.decode()
+            if line.startswith("#"):
+                # hold tags until we find a segment
+                results.append(line)
+            else:
+                tp=TagParser(results)
+                # when we find a segment, make it a key in data
+                # and a dict of the tags as the value
+                # trim down to just the file name.
+                # "https://example.com/seg137.ts?fu=you" --> "seg137.ts"
+                line = line.rsplit('/',1)[-1].split('?',1)[0].strip()
+                if line:
+                    # segment name is the key and the tags are the value.
+                    data[line]=tp.tags
+                    # clear results for next segment
+                    results =[]
+    if data:
+        print(json.dumps(data, indent=4))
