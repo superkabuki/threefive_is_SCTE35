91ac51b84862755a93bacfa93b1c0e60b91c329e
Super Kabuki <a@iodisco.com>
2025-05-12 13:43:12 -0400

    Update edit_break_duration.py

--- examples/edit_break_duration.py
+++ examples/edit_break_duration.py
@@ -2,13 +2,13 @@
 Example of Editing a Cue.Command and re-encoding
 """
 
-import threefive3
+import threefive
 
 # Base64 encoded SCTE-35 Cue
 BE64 = "/DAvAAAAAAAA///wFAVIAACPf+/+c2nALv4AUsz1AAAAAAAKAAhDVUVJAAABNWLbowo="
 
-# Create a threefive3 instance with BE64
-cue = threefive3.Cue(BE64)
+# Create a threefive instance with BE64
+cue = threefive.Cue(BE64)
 
 # print the  results as JSON
 cue.show()
@@ -17,7 +17,7 @@ print(f"\n\nStarting Break Duration was {cue.command.break_duration}\n\n")
 cue.command.break_duration = 120.0
 
 # Run cue.encode to generate new base64 string
-# threefive3 will automatically re-calculate the crc and length vars for you.
+# threefive will automatically re-calculate the crc and length vars for you.
 cue.encode()
 
 # print the new results as JSON
