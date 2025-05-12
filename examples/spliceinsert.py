ef72d3b3bcc0ddee492cb5f68ff8d96acd741fc0
Super Kabuki <a@iodisco.com>
2025-05-12 13:46:40 -0400

    Update spliceinsert.py

--- examples/spliceinsert.py
+++ examples/spliceinsert.py
@@ -5,7 +5,7 @@ Example from the specification
 
 """
 
-import threefive3
+import threefive
 
 
 HEX = "0xFC302F000000000000FFFFF014054800008F7FEFFE7369C02EFE0052CCF500000000000A0008435545490000013562DBA30A"
@@ -13,16 +13,16 @@ B64 = "/DAvAAAAAAAA///wFAVIAACPf+/+c2nALv4AUsz1AAAAAAAKAAhDVUVJAAABNWLbowo="
 
 # Using Hex
 
-hex2cue = threefive3.Cue(HEX)
+hex2cue = threefive.Cue(HEX)
 
 
 # Using Base64
 # (output should be the same as above)
 
-base642cue=threefive3.Cue(B64)
+base642cue=threefive.Cue(B64)
 
-threefive3.blue("Cue from Hex")
+threefive.blue("Cue from Hex")
 hex2cue.show()
 
-threefive3.blue("Cue from Base64")
+threefive.blue("Cue from Base64")
 base642cue.show()
