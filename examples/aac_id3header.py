7307c54b431676f0dc30c5543884432f1cb4ae62
Super Kabuki <a@iodisco.com>
2025-05-12 13:40:46 -0400

    Update aac_id3header.py

--- examples/aac_id3header.py
+++ examples/aac_id3header.py
@@ -2,12 +2,12 @@
 """
 aac_id3header.py
 
-Use threefive3.aac.AacParser class to 
+Use threefive.aac.AacParser class to 
 parse HLS aac segments for PTS in ID3 header tags. 
 
 """
 
-from threefive3.aac import AacParser
+from threefive.aac import AacParser
 
 ap = AacParser()
 #  this is an HLS audio only segment with PTS in an ID3 header tag.
