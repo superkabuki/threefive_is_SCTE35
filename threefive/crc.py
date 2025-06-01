5f775877ca366d1e6a5e01f1145959b56734558c
Super Kabuki <a@iodisco.com>
2025-06-01 16:44:41 -0400

    fmt

--- threefive/crc.py
+++ threefive/crc.py
@@ -4,10 +4,10 @@ crc.py  crc32 function for encoding.
 
 from .crctable import CRCTABLE
 
-INIT_VALUE =      0xFFFFFFFF
-GONZO =              0xFFFFFF00
-EIGHT =                 0x08
-TWENTYFOUR =  0x18
+INIT_VALUE = 0xFFFFFFFF
+GONZO =  0xFFFFFF00
+EIGHT = 0x08
+TWENTYFOUR = 0x18
 TWOFIFTYFIVE = 0xFF
 
 
