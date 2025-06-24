3e4478448703eb140f62ee977c5ec5b840c26ac0
Super Kabuki <a@iodisco.com>
2025-06-24 04:06:30 -0400

    Update stuff.py

--- threefive/stuff.py
+++ threefive/stuff.py
@@ -1,7 +1,7 @@
 """
 stuff.py functions and such common to threefive.
 
-print2, atohif, iso8601, red, blue
+print2, bahtoif, clean, codec_detect, isfloat,ishex, isjson, isxml, iso8601, k_by_v, red,rmap, blue, reblue, print2, and ERR
 """
 
 import datetime
@@ -18,7 +18,107 @@ ERR = (
     KeyError,
 )
 
+
 write2 = True
+
+
+def codec_detect(data):
+    """
+    codec_detect decode bytes by trying multiple encodings.
+    """
+    codecs = [
+        "utf8",
+        "latin1",
+        "ascii",
+        "big5",
+        "euc_kr",
+        "koi8_r",
+        "koi8_t",
+        "cp437",
+        "cp1250",
+        "cp1251",
+        "utf16",
+        "utf32",
+    ]
+    for codec in codecs:
+        try:
+            data = data.decode(encoding=codec)
+            return codec, data
+        except:
+            pass
+
+
+def clean(data):
+    """
+    clean strip and if it's a byte string
+    convert to a string
+    """
+    if isinstance(data, bytes):
+        codec, data = codec_detect(data)
+    if not isinstance(data, str):
+        badtype(data, str)
+    else:
+        data = data.strip()
+    return data
+
+
+def ishex(data):
+    """
+    ishex determine if a string is a hex value.
+    """
+    data=clean(data)
+    hexed = "0123456789abcdef"
+    data = data.lower().strip("0x")
+    return all([c in hexed for c in data])
+    #return False
+
+
+def isjson(data):
+    """
+    isjson determine if a string or bytestring
+    is json.
+    """
+    data = clean(data)
+    if data[0] in ["{"]:
+        if data[-1] in ["}"]:
+            return True
+    return False
+
+
+def isfloat(value):
+    """
+    isfloat determine if a str or bytes is a float 
+    """
+    value = clean(value)
+    return "." in value and value.replace(".", "").isdigit()
+
+
+def isxml(data):
+    """
+    isxml determine if a string or bytestring
+    is xml.
+    """
+    data = clean(data)
+    if data[0] in ["<"]:
+        if data[-1] in [">"]:
+            return True
+    return False
+
+
+def bahtoif(value):
+    """
+    bahtoif converts bytes, ascii, or hex to int or  float.
+    """
+    value= clean(value)
+    value = value.strip()
+    value = value.strip(",")
+    if value.isdigit():
+        value = int(value)
+    elif ishex(value):
+        value = int(value, 16)
+    elif isfloat(value):
+        value = float(value)
+    return value
 
 
 def k_by_v(adict, avalue):
@@ -77,101 +177,6 @@ def print2(gonzo=b""):
         print(gonzo, file=stderr, flush=True)
     else:
         print(gonzo)
-
-
-def _isfloat(value):
-    return "." in value and value.replace(".", "").isdigit()
-
-
-def atohif(value):
-    """
-    atohif converts ascii to (hex|int|float)
-    """
-    if isinstance(value, str):
-        value = value.strip()
-        value = value.strip(",")
-        if "0x" in value.lower():
-            value = int(value, 16)
-        elif value.isdigit():
-            value = int(value)
-        elif _isfloat(value):
-            value = float(value)
-    return value
-
-
-def codec_detect(data):
-    """
-    codec_detect decode bytes by trying multiple encodings.
-    """
-    codecs = [
-        "utf8",
-        "latin1",
-        "ascii",
-        "big5",
-        "euc_kr",
-        "koi8_r",
-        "koi8_t",
-        "cp437",
-        "cp1250",
-        "cp1251",
-        "utf16",
-        "utf32",
-    ]
-    for codec in codecs:
-        try:
-            data = data.decode(encoding=codec)
-            return codec, data
-        except:
-            pass
-
-
-def clean(data):
-    """
-    clean strip and if it's a byte string
-    convert to a string
-    """
-    if isinstance(data, bytes):
-        codec, data = codec_detect(data)
-    if not isinstance(data, str):
-        badtype(data, str)
-    else:
-        data = data.strip()
-    return data
-
-
-def ishex(data):
-    """
-    ishex determine if a string is a hex value.
-    """
-    if isinstance(data, str):
-        hexed = "0123456789abcdef"
-        data = data.lower().strip("0x")
-        return all([c in hexed for c in data])
-    return False
-
-
-def isjson(data):
-    """
-    isjson determine if a string or bytestring
-    is json.
-    """
-    data = clean(data)
-    if data[0] in ["{", b"{"]:
-        if data[-1] in ["}", b"}"]:
-            return True
-    return False
-
-
-def isxml(data):
-    """
-    isxml determine if a string or bytestring
-    is xml.
-    """
-    data = clean(data)
-    if data[0] in ["<", b"<"]:
-        if data[-1] in [">", b">"]:
-            return True
-    return False
 
 
 def iso8601():
