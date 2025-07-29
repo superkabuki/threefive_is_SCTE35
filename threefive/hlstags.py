d278b33d19e61d146e26988c1bbb057d4d4fd40a
Super Kabuki <a@iodisco.com>
2025-07-28 22:40:32 -0400

    Critical fix for EXT-X-DATERANGE tag parsing

--- threefive/hlstags.py
+++ threefive/hlstags.py
@@ -122,7 +122,7 @@ class TagParser:
             return
         tag, tail = line.split(colon, one)
         self.tags[tag] = {}
-        if tag in ["#EXT-OATCLS-SCTE35","#EXT-X-DATERANGE"]:
+        if tag in ["#EXT-OATCLS-SCTE35"]:
             self._oated(tag, tail)
             return
         self._split_tail(tag, tail)
