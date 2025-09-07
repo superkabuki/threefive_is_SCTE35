572a2c1cf032924b4fb385028003f71b639cd9e6
Super Kabuki <a@iodisco.com>
2025-09-07 08:12:13 -0400

    black

--- examples/base64toxmlbin.py
+++ examples/base64toxmlbin.py
@@ -8,18 +8,16 @@ and then convert the xml+binary back to base64
 from threefive import Cue
 
 
-HEAD ='\n\033[107;2;36m '
-TAIL = '\033[0m\n'
+HEAD = "\n\033[107;2;36m "
+TAIL = "\033[0m\n"
 
 
 if __name__ == "__main__":
 
-
-
     #  Base64 To Xml .
     b64 = "/DAWAAAAAAAAAP/wBQb+z26yLwAAXeqFJg=="
     cue = Cue(b64)  # initialize a Cue instance with the base64 string.
-    exbin= cue.xmlbin()  # call the Cue.xmlbin() method
+    exbin = cue.xmlbin()  # call the Cue.xmlbin() method
     print(f"{HEAD} base64  -> xmlbin {TAIL} ")
     print(exbin)
 
