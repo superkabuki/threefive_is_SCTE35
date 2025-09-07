89c8a140b63791a1671c63b2d62c069d46cdd62f
Super Kabuki <a@iodisco.com>
2025-09-07 07:36:37 -0400

    Update words.py

--- threefive/words.py
+++ threefive/words.py
@@ -1,60 +1,60 @@
 """
-threefive.words words.py
+THREEFIVE.words words.py
 lowercase constant word names for numbers and punctuation.
 
 """
 
 # Numbers
-minusone = -0x1
-zero = 0x0
-one = 0x1
-two = 0x2
-three = 0x3
-four = 0x4
-five = 0x5
-six = 0x6
-seven = 0x7
-eight = 0x8
-nine = 0x9
-ten = 0xA
-eleven = 0xB
-twelve = 0xC
-thirteen = 0xD
-fourteen = 0xE
-fifteen = 0xF
-sixteen = 0x10
-seventeen = 0x11
-eighteen = 0x12
-nineteen = 0x13
-twenty = 0x14
-twentyone = 0x15
-twentytwo = 0x16
-twentythree = 0x17
-twentyfour = 0x18
-twentyfive = 0x19
-twentysix = 0x1A
-twentyseven = 0x1B
-twentyeight = 0x1C
-twentynine = 0x1D
-thirty = 0x1E
-thirtyone = 0x1F
-thirtytwo = 0x20
-thirtythree = 0x21
-sixtyfour = 0x40
-onetwentyeight = 0x80
-twofiftyfive = 0xFF
-twofiftysix = 0x100
-ninetythousand = 90000.0
+MINUSONE = -0x1
+ZERO = 0x0
+ONE = 0x1
+TWO = 0x2
+THREE = 0x3
+FOUR = 0x4
+FIVE = 0x5
+SIX = 0x6
+SEVEN = 0x7
+EIGHT = 0x8
+NINE = 0x9
+TEN = 0xA
+ELEVEN = 0xB
+TWELVE = 0xC
+THIRTEEN = 0xD
+FOURTEEN = 0xE
+FIFTEEN = 0xF
+SIXTEEN = 0x10
+SEVENTEEN = 0x11
+EIGHTEEN = 0x12
+NINETEEN = 0x13
+TWENTY = 0x14
+TWENTYONE = 0x15
+TWENTYTWO = 0x16
+TWENTYTHREE = 0x17
+TWENTYFOUR = 0x18
+TWENTYFIVE = 0x19
+TWENTYSIX = 0x1A
+TWENTYSEVEN = 0x1B
+TWENTYEIGHT = 0x1C
+TWENTYNINE = 0x1D
+THIRTY = 0x1E
+THIRTYONE = 0x1F
+THIRTYTWO = 0x20
+THIRTYTHREE = 0x21
+SIXTYFOUR = 0x40
+ONETWENTYEIGHT = 0x80
+TWOFIFTYFIVE = 0xFF
+TWOFIFTYSIX = 0x100
+NINETYTHOUSAND = 90000.0
 
 # Quotes
-dblquote = '"'
-quote = "'"
-nothing = ""
+DBLQUOTE = '"'
+QUOTE = "'"
+NOTHING = ""
 
 
 # Other
-comma = ","
-colon = ":"
-equalsign = "="
-octothorpe = "#"
-space = " "
+COMMA = ","
+COLON = ":"
+EQUALSIGN = "="
+OCTOTHORPE = "#"
+SPACE = " "
