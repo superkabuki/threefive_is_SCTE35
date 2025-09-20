026c14f9142e7960128abfed639cce0965d55d4b
Super Kabuki <a@iodisco.com>
2025-09-20 04:03:35 -0400

    fast SCTE-35 pts adjustment

--- threefive/bump.py
+++ threefive/bump.py
@@ -18,13 +18,13 @@ if the Cue in the packet doesn't have cue.command.pts_time:
 final values are modolo`ed to the ROLLOVER.
 
 """
+
 import io
-
 import sys
-
 from .cue import Cue
 from .stuff import blue
 from .stream import Stream
+
 
 ROLLOVER = 95443.717678
 
@@ -46,27 +46,16 @@ def bump_pts_adjust(cue, bump):
     cue.info_section.pts_adjustment = bumped % ROLLOVER
 
 
-def show_bump(pay, cue):
-    """
-    show_bump show the Cue with adjusted pts.
-    """ 
-    blue("Cue adjusted")
-   # cue.show()
-    #blue(f"stop {len(pay)}")
-
-
 def bump_pts(pay, bump):
     """
     bump_pts adjust SCTE-35 pts by bump
     """
-    #blue(f" start {len(pay)}")
     cue = Cue(pay)
     if cue.command.pts_time:
         bump_pts_time(cue, bump)
     else:
         bump_pts_adjust(cue, bump)
-    bites=cue.bytes()
-    show_bump(pay, cue)
+    cue.encode()
     return cue.bytes()
 
 
@@ -77,7 +66,6 @@ def repad(pkt):
     pad = b"\xff"
     padsize = 188 - len(pkt)
     pkt = pkt + (pad * padsize)
-   # blue(f"pkt {len(pkt)}")
     return pkt
 
 
@@ -90,7 +78,6 @@ def bumped(pkt, bump):
         pre = pkt.split(b"\x00\x00\x01\xfc")[-1]
         tail = pre[pre.index(b"\xfc") :]
         head = pkt.replace(tail, b"")
-        #blue(f"head {len(head)}")
         tail = bump_pts(tail, bump)
         pkt = repad(head + tail)
     return pkt
@@ -103,15 +90,9 @@ class BumpStream(Stream):
         Adjust SCTE-35 PTS times  in MPEGTS
     """
 
-    def _pts(self, pkt, pid):
-        if self._pusi_flag(pkt):
-            self._chk_pts(pkt, pid)
-
     def _scte35(self, pkt, pid, bump):
         if self._pid_has_scte35(pid):
             cue = self._parse_scte35(pkt, pid)
-##            if cue:
-##                cue.show()
             pkt = bumped(pkt, bump)
         return pkt
 
@@ -122,10 +103,8 @@ class BumpStream(Stream):
         return modified pkt.
         """
         pid = self._parse_info(pkt)
-      #  self._pts(pkt, pid)
         pkt = self._scte35(pkt, pid, bump)
         return pkt
-
 
     def bump_scte35(self, bump):
         """
@@ -134,10 +113,9 @@ class BumpStream(Stream):
         bump = float(bump)
         num_pkts = 2420
         for chunk in self.iter_pkts(num_pkts):
-            pkts = [self.parse(chunk[i : i + self.PACKET_SIZE], bump)
-             for i in range(0, len(chunk), self.PACKET_SIZE)
+            pkts = [
+                self.parse(chunk[i : i + self.PACKET_SIZE], bump)
+                for i in range(0, len(chunk), self.PACKET_SIZE)
             ]
             sys.stdout.buffer.write(b"".join(pkts))
-            sys.stdout.buffer.flush()
-
-        return False
+ 
