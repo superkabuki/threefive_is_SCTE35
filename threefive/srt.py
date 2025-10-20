55f0bfc3184146aba4be40dc590ebd22253fa441
Super Kabuki <a@iodisco.com>
2025-10-19 23:51:59 -0400

    trying to class up the joint.

--- threefive/srt.py
+++ threefive/srt.py
@@ -3,14 +3,13 @@ threefive/srt.py
 implementing Secure Reliable Transport protocol via ctypes and  libsrt.so
 
 this is not fully functioning yet.
-
-
 """
 
 import ctypes
 import sys
 import socket
 
+SIZE64=64
 SRTO_SENDER = 21
 PORT = 8080
 ADDR = "127.0.0.1"
@@ -36,26 +35,35 @@ class sockaddr_in(ctypes.Structure):
     ]
 
 
-# Create an instance of sockaddr_in
-sa_in = sockaddr_in()
-sa_in.sin_family = socket.AF_INET
-sa_in.sin_port = socket.htons(PORT)
-sa_in.sin_addr.s_addr = int.from_bytes(socket.inet_aton(ADDR))  # IPv4 address
+def  ipv4int(addr):
+    """
+    take a ipv4 string addr and make it an int
+    """
+    return  int.from_bytes(socket.inet_aton(addr)) 
 
-# Get a pointer to sa_in
-sa_in_ptr = ctypes.pointer(sa_in)
 
-# This is the ctypes equivalent of (struct sockaddr*)&sa
-sa_ptr = ctypes.cast(sa_in_ptr, ctypes.POINTER(sockaddr))
+def mk_sockaddr_sa(addr,port):
+    """
+    mk_sockaddr_sa make a c compatible (struct sockaddr*)&sa
+    """
+    sa_in = sockaddr_in()
+    sa_in.sin_family = socket.AF_INET
+    sa_in.sin_port = socket.htons(port)
+    sa_in.sin_addr.s_addr = ipv4int(addr)
+    # Get a pointer to sa_in
+    sa_in_ptr = ctypes.pointer(sa_in)
+    #  (struct sockaddr*)&sa
+    return ctypes.cast(sa_in_ptr, ctypes.POINTER(sockaddr))
+   
 
+sa =mk_sockaddr_sa(ADDR,PORT)
 libsrt = ctypes.CDLL("libsrt.so")
 libsrt.srt_startup()
 ss = libsrt.srt_create_socket()
 libsrt.srt_setsockflag(ss, SRTO_SENDER, 1, 8)
-st = libsrt.srt_connect(
-    ss, sa_ptr, 64
-)  # How big is a pointer? 32? I went 64 just in case.
+st = libsrt.srt_connect(ss, sa, SIZE64)
+
+# works at least up to here.
 
 st = libsrt.srt_close(ss)
-
-srt_cleanup()
+libsrt.srt_cleanup()
