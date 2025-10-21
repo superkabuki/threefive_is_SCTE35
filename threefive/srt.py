8343b3c7cdc06d45152551784b2d8c0905259c4f
Super Kabuki <a@iodisco.com>
2025-10-21 09:30:28 -0400

    srt  startup, sockets, messages, cleanup and close all working.

--- threefive/srt.py
+++ threefive/srt.py
@@ -3,16 +3,21 @@ threefive/srt.py
 implementing Secure Reliable Transport protocol via ctypes and  libsrt.so
 
 this is not fully functioning yet.
+
+
 """
 
 import ctypes
 import sys
 import socket
+import time
 
-SIZE64=64
+YES = 1
 SRTO_SENDER = 21
-PORT = 8080
+PORT = 9000
 ADDR = "127.0.0.1"
+
+MSG = ctypes.create_string_buffer(b"bigcorp message", 15)
 
 
 class sockaddr(ctypes.Structure):
@@ -23,26 +28,28 @@ class sockaddr(ctypes.Structure):
 
 
 class in_addr(ctypes.Structure):
-    _fields_ = [("s_addr", ctypes.c_uint)]  # IPv4 address
+    _fields_ = [("s_addr", ctypes.c_uint32)]  # IPv4
 
 
 class sockaddr_in(ctypes.Structure):
     _fields_ = [
-        ("sin_family", ctypes.c_ushort),
+        ("sin_family", ctypes.c_short),
         ("sin_port", ctypes.c_ushort),
         ("sin_addr", in_addr),
-        ("sin_zero", ctypes.c_char * 8),  # Padding
+        ("sin_zero", ctypes.c_char * 8),
     ]
 
 
-def  ipv4int(addr):
+def ipv4int(addr):
     """
     take a ipv4 string addr and make it an int
     """
-    return  int.from_bytes(socket.inet_aton(addr)) 
+    sa = int.from_bytes(socket.inet_pton(socket.AF_INET, addr), byteorder="little")
+    print(f"SA {sa}")
+    return sa
 
 
-def mk_sockaddr_sa(addr,port):
+def mk_sockaddr_sa(addr, port):
     """
     mk_sockaddr_sa make a c compatible (struct sockaddr*)&sa
     """
@@ -50,20 +57,38 @@ def mk_sockaddr_sa(addr,port):
     sa_in.sin_family = socket.AF_INET
     sa_in.sin_port = socket.htons(port)
     sa_in.sin_addr.s_addr = ipv4int(addr)
+    # socket.inet_pton(socket.AF_INET, addr)
     # Get a pointer to sa_in
     sa_in_ptr = ctypes.pointer(sa_in)
     #  (struct sockaddr*)&sa
-    return ctypes.cast(sa_in_ptr, ctypes.POINTER(sockaddr))
-   
+    return ctypes.cast(sa_in_ptr, ctypes.POINTER(sockaddr)), ctypes.sizeof(sa_in)
 
-sa =mk_sockaddr_sa(ADDR,PORT)
+
 libsrt = ctypes.CDLL("libsrt.so")
-libsrt.srt_startup()
-ss = libsrt.srt_create_socket()
-libsrt.srt_setsockflag(ss, SRTO_SENDER, 1, 8)
-st = libsrt.srt_connect(ss, sa, SIZE64)
+libsrt.srt_getlasterror_str.restype = ctypes.c_char_p
 
-# works at least up to here.
+libsrt.srt_startup()
+sa_ptr, sa_size = mk_sockaddr_sa(ADDR, PORT)
+print(f"Make Socket Addr {libsrt.srt_getlasterror_str()}")
+
+ss = libsrt.srt_create_socket()
+print(f"Create Socket: {libsrt.srt_getlasterror_str()}")
+
+libsrt.srt_setsockflag(ss, SRTO_SENDER, 1, 32)
+print(f"Set Socket: {libsrt.srt_getlasterror_str()}")
+
+st = libsrt.srt_connect(ss, sa_ptr, 64)
+if st:
+    print(f"Connect: {libsrt.srt_getlasterror_str()}")
+loops = 100
+while loops:
+    loops -= 1
+    st = libsrt.srt_sendmsg2(ss, MSG, ctypes.sizeof(MSG), None)
+    if st:
+        print(f"Sending Message: {libsrt.srt_getlasterror_str()}")
+    time.sleep(0.1)
+
+time.sleep(1)
 
 st = libsrt.srt_close(ss)
 libsrt.srt_cleanup()
